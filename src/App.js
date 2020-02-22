26f44325cc3d8e6cc4facfff723e702c80f3a43c
Jordy Shores <jordan.binskin@gmail.com>
2020-02-23 10:09:46 +1100

    basic styling and mvp of article preview

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Button from './components/Button';
 import ArticlePreview from './components/ArticlePreview'
+import data from './dummy.js'
 
 function App() {
   return (
     <div className="App">
-      <ArticlePreview />
+      {
+        data.map(article => <ArticlePreview heading={article.heading} image={article.image} blurb={article.blurb} />)
+      }
       <Button text={'botton text'} />
     </div>
   );
--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+const readTime = words => `${words.length / 200}`;
+
 const ArticlePreview = props => {
   return (
-    <div>
-      I'm an article preview
-    </div>
+    <a className="article-preview">
+      <h2>{props.heading}</h2>
+      <img width="250px" height="250px" src={props.image}/>
+      <p>{props.blurb}</p>
+      <p>Approx read time: {readTime(props.blurb)}</p>
+    </a>
   )
 }
 
