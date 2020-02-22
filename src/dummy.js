91de9fb31536ddf0061922bb6f8ae89b314bb7bb
Jordy Shores <jordan.binskin@gmail.com>
2020-02-23 10:17:13 +1100

    adding article data

---
+++ src/dummy.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    "heading": "How to meme",
+    "image": "http://dummyimage.com/157x110.bmp/dddddd/000000",
+    "blurb": "A full interactive guide on how to be a good memer"
+  },
+  {
+    "heading": "I like to yeet",
+    "image": "http://dummyimage.com/143x179.jpg/ff4444/ffffff",
+    "blurb": "A full approach on how to keep on yeeting"
+  },
+  {
+    "heading": "The meme that never gets old",
+    "image": "http://dummyimage.com/233x215.jpg/cc0000/ffffff",
+    "blurb": "How to explain to your friend they should stop using that meme without saying you hate them"
+  },
+  {
+    "heading": "Canada",
+    "image": "http://dummyimage.com/173x117.jpg/dddddd/000000",
+    "blurb": "A beautiful place"
+  }
+]
